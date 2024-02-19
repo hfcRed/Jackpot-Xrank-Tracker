18237876972d908227bb798769627c8e7aee2291
hfcRed <hfcred@gmx.net>
2024-02-19 21:24:47 +0100

    Add basic index styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				textPrimary: '#FEFEFE',
+				textSecondary: '#AFAFB2',
+				background: '#0E0E0E',
+			},
+		},
 	},
 	plugins: [],
 }
