a5960a609b22f5649e4b0cfedd2e115721f8f103
hfcRed <hfcred@gmx.net>
2024-02-29 21:42:00 +0100

    Overhaul styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,10 +6,12 @@ export default {
 			colors: {
 				textPrimary: '#FEFEFE',
 				textSecondary: '#AFAFB2',
-				background: '#1A1212',
-				backgroundDark: '#0D0909',
-				backgroundLight: '#261B1B',
-				backgroundLighter: '#663D3D',
+				background: '#1C1E2B',
+				backgroundDark: '#161821',
+				backgroundDarker: '#11121A',
+				backgroundLight: '#252433',
+				backgroundLighter: '#46394C',
+				border: '#393C4C',
 			},
 			fontFamily: {
 				'Blitz': ['Blitz'],
@@ -22,6 +24,12 @@ export default {
 				'logo': ['20px 20px 100px #ff00ff', '0px 0px 50px #ff00ff'],
 				'crown': ['0px 0px 100px #F54910', '0px 0px 50px #F54910', '0px 150px 100px #F54910', '0px 250px 100px #F51081'],
 				'filter': ['0px -75px 100px rgba(245, 16, 16, 0.25)'],
+			},
+			borderWidth: {
+				'1': '1px',
+			},
+			backgroundImage: {
+				shine: 'radial-gradient(500px 100px at 50% 0%, rgba(55, 55, 75, 0.25) 0%, rgba(20, 30, 50, 0) 100%)'
 			}
 		},
 	},
